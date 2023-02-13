b44d9a838720c26ae485ae6bb66a8ae179fe3273
Pedro Guedes <pedroguedes@Pedros-MacBook-Air.local>
2023-02-13 20:40:07 +0100

    improvements to deplyment

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(express.static(path.join(__dirname, 'client/public')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'client/public', 'index.html'));
