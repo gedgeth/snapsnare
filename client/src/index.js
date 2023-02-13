bfd8b26b843aaf20115250004134858d80c86927
Pedro Guedes <pedroguedes@Pedros-MacBook-Air.local>
2023-02-13 20:56:34 +0100

    Improvements to index.js

--- client/src/index.js
+++ client/src/index.js
@@ -2,4 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import TweetForm from './TweetForm';
 
+app.get('/index.js', (req, res) => {
+    res.set('Content-Type', 'text/javascript');
+    res.sendFile(path.join(__dirname, 'client/build/static/js', 'main.52c91760.js'));
+  });
+  
+
 ReactDOM.render(<TweetForm />, document.getElementById('root'));
