a6d1d190ba778d76338268b6dbefdc664a3abc41
Pedro Guedes <pedroguedes@Pedros-MacBook-Air.local>
2023-02-13 20:59:57 +0100

    Improvements to index.js

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ const port = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/public', 'index.html'));
+app.get('/index.js', (req, res) => {
+  res.set('Content-Type', 'text/javascript');
+  res.sendFile(path.join(__dirname, 'client/build/static/js', 'main.52c91760.js'));
 });
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
