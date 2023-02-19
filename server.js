28abb75c7a366743cb1f2f6f8eba9e1d26aa72bf
Pedro Guedes <pedroguedes@Pedros-Air.home>
2023-02-19 21:42:12 +0000

    commit message

--- server.js
+++ server.js
@@ -1,22 +1,39 @@
+const express = require('express');
 const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(express.static(__dirname + '/client/build'));
-app.use(express.static(__dirname + '/public'));
+const path = require('path');
+const Twit = require('twit');
 
+const app = express();
+const port = process.env.PORT || 5000;
+
+const T = new Twit({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token: process.env.TWITTER_ACCESS_TOKEN,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+});
+
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// Parse incoming requests data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Set the MIME type for index.js
 app.use((req, res, next) => {
-  res.header('Content-Type', 'text/javascript');
+  if (req.url.endsWith('/index.js')) {
+    res.set('Content-Type', 'text/javascript');
+  }
   next();
 });
 
-
+// Serve the index.js file
 app.get('/static/js/index.js', (req, res) => {
-  res.set('Content-Type', 'text/javascript');
-  res.sendFile(path.join(__dirname, 'client', 'build', 'static', 'js', 'index.js'));
+  res.sendFile(path.join(__dirname, 'client/build', 'static/js/index.js'));
 });
 
-
-
+// Endpoint for posting a tweet
 app.post('/api/tweet', (req, res) => {
   const tweet = req.body.tweet;
   // Use the Twit library to post the tweet
@@ -29,4 +46,13 @@ app.post('/api/tweet', (req, res) => {
       res.send('Tweet posted successfully');
     }
   });
+});
+
+// Handles any requests that don't match the ones above
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
 });
